bd4988cb0bfc5887f2a0cbebd17367ea36f4a4ad
Priyadharshini-S1 <133557905+Priyadharshini-S1@users.noreply.github.com>
2024-02-23 19:00:46 +0530

    Add files via upload

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { PortfolioService } from '../portfolio.service';
 @Component({
@@ -6,6 +6,21 @@ import { PortfolioService } from '../portfolio.service';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
-
+export class AboutComponent implements OnInit{
+  ngOnInit(): void {
+    const counters = document.querySelectorAll(".counter");
+    counters.forEach((counter:any) => {
+      counter.innerText = "0";
+      const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+        const increment = target / 200;
+        if (count < target) {
+          counter.innerText = `${Math.ceil(count + increment)}`;
+          setTimeout(updateCounter, 1);
+        } else counter.innerText = target;
+      };
+      updateCounter();
+    });
+  }
 }
---
+++ src/app/testimonals/testimonals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonals',
+  templateUrl: './testimonals.component.html',
+  styleUrl: './testimonals.component.css'
+})
+export class TestimonalsComponent {
+
+}
